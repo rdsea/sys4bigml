eb894eba5453778e99f0d18280ef1de2aaf09185
linhnguyen222 <nguyenmylinh.9720@gmail.com>
2021-07-23 16:04:28 +0300

    testing serving with mlflow

--- tutorials/MLProjectManagement/BTS_Example/model.py
+++ tutorials/MLProjectManagement/BTS_Example/model.py
@@ -4,9 +4,13 @@ import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-from tensorflow.keras.layers.experimental import preprocessing
 from sklearn.metrics import mean_squared_error
 import mlflow
+import sys
+# from urllib.parse import urlparse
+import mlflow.keras
+
+import logging
 
 # Read dataset from file
 raw_dataset = pd.read_csv("./data_grouped/1161114002_122_.csv")
@@ -23,8 +27,8 @@ test_dataset = test_raw_dataset.copy()
 test_dataset = test_dataset.dropna().drop(['id','station_id','parameter_id','unix_timestamp'], axis=1)
 test_dataset_full = test_dataset.sort_values(by=['norm_time'])
 # Choose a small part of the data to test the model
-start_line = 40
-end_line = 100
+start_line = int(sys.argv[1]) if len(sys.argv) > 1 else 40
+end_line = int(sys.argv[2]) if len(sys.argv) > 2 else 100
 test_data = test_dataset_full[start_line:end_line]
 
 # pre-processing data 
@@ -48,14 +52,17 @@ test_serial_data['norm_6'] = test_serial_data['norm_value'].shift(6)
 test_serial_data = test_serial_data[6:]
 train_dataset = serial_data
 test_dataset = test_serial_data
+
 train_features = np.array(train_dataset.drop(['norm_value'], axis=1))
 train_features = np.array(train_features)[:,:,np.newaxis]
 train_labels = np.array(train_dataset.drop(['norm_6'], axis=1))
 train_labels = train_labels.reshape(train_labels.shape[0],train_labels.shape[1],1)
 test_features = np.array(test_dataset.drop(['norm_value'], axis=1))
 test_features = test_features.reshape(test_features.shape[0],test_features.shape[1],1)
+
 test_labels = np.array(test_dataset.drop(['norm_6'], axis=1))
 test_labels = test_labels.reshape(test_labels.shape[0],test_labels.shape[1],1)
+
 
 with mlflow.start_run():
 
@@ -64,9 +71,11 @@ with mlflow.start_run():
     model.add(layers.LSTM(32, return_sequences=True))
     model.add(layers.TimeDistributed(layers.Dense(1)))
     model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(0.005))
-    model.fit(train_features, train_labels, epochs=200, batch_size=1, verbose=2)
+    
+    model.fit(train_features, train_labels, epochs=2, batch_size=1, verbose=2)
     
     result = model.predict(test_features, batch_size=1, verbose=0)
+    print(test_features)
     x=pd.DataFrame(test_labels.reshape(test_labels.shape[0],test_labels.shape[1]))
     y=pd.DataFrame(result.reshape(result.shape[0],result.shape[1]))
     y_true = np.array(x[0])
@@ -85,5 +94,8 @@ with mlflow.start_run():
     plt.legend()
     plt.savefig("BTS_resultGraph.png")
     mlflow.log_artifact("BTS_resultGraph.png")
-    plt.show()
-    plt.close()
+    mlflow.keras.log_model(model, "LSTM_model")
+    # # plt.show()
+    # plt.close()
+    model_dir_path = "./saved_model"
+    # mlflow.keras.save_model(model, model_dir_path)
