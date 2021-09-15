3a5e85ab9fc12f360cf110c86de10ecf900910bc
linhnguyen222 <nguyenmylinh.9720@gmail.com>
2021-09-15 13:34:38 +0300

    update prometheus presentation

---
+++ tutorials/PerformanceMonitoring/MLService/resource_management.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+import psutil
+app = Flask(__name__)
+
+@app.route("/metrics")
+def log_metric_prometheus():
+    metric = 'cpu_percent {}\n'.format(psutil.cpu_percent())
+    metric += 'virtual_memory_free {}\n'.format(psutil.virtual_memory()[4])
+    metric += 'virtual_memory_percent {}\n'.format(psutil.virtual_memory()[2])
+    return metric
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8888, debug=True)
