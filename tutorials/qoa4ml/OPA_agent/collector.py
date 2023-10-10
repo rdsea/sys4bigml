28cb200f5ff2f534ac08e68d6e05a2527717e65d
Tri Nguyen <tri.m.nguyen@aalto.fi>
2023-10-10 14:55:40 +0300

    update qoa4ml tutorial

--- tutorials/qoa4ml/OPA_agent/collector.py
+++ tutorials/qoa4ml/OPA_agent/collector.py
@@ -1,5 +1,6 @@
 from qoa4ml.collector.amqp_collector import Amqp_Collector
-import qoa4ml.utils as utils
+from qoa4ml import qoaUtils as utils
+
 import json, copy
 import pandas as pd
 from os.path import exists as file_exists
@@ -41,55 +42,53 @@ class Monitoring_collector(object):
     def message_processing(self, ch, method, props, body):
         mess = json.loads(str(body.decode("utf-8")))
         app_metric = None
-        if 'App-metric' in mess:
-            app_metric = mess.pop('App-metric')
+        if 'quality' in mess:
+            app_metric = mess.pop('quality')
             for key in app_metric:
                 mess[key] = app_metric[key]
             report = self.report
-            report['client_info']['id'] = mess['client_id']
+
+            
+            # This code is sending default report to OPA
+            # TO DO:
+            # Get quality metric from "mess" and send to OPA
+
+ 
+            report['client_info']['id'] = mess['metadata']['client_id']
             qoa_response = self.opa_client.check_policy_rule(input_data=report, package_path="qoa4ml.object_detection", rule_name="violation")
             print(qoa_response)
 
-        if 'Process-metric' in mess:
-            process_metric = mess.pop('Process-metric')
+
+        if 'proc_cpu_stats' in mess:
+            process_metric = mess.pop('proc_cpu_stats')
             for key in process_metric:
                 mess[key] = process_metric[key]
-            client_id = mess['client_id']
-            service_id = mess['client_id']
-            instance_id = mess['instance_id']
+            client_id = mess['metadata']['client_id']
+            stage_id = mess['metadata']['stage_id']
+            instance_id = mess['metadata']['instance_name']
             
             if not (client_id in self.proc_cpu_time):
                 self.proc_cpu_time[client_id] = {}
-            if not (service_id in self.proc_cpu_time[client_id]):
-                self.proc_cpu_time[client_id][service_id] = {}
-            if not (instance_id in self.proc_cpu_time[client_id][service_id]):
-                self.proc_cpu_time[client_id][service_id][instance_id] = Gauge('process_cpu_time_'+str(self.prom_id), "CPU-Time of process in user mod of "+str(instance_id))
+            if not (stage_id in self.proc_cpu_time[client_id]):
+                self.proc_cpu_time[client_id][stage_id] = {}
+            if not (instance_id in self.proc_cpu_time[client_id][stage_id]):
+                self.proc_cpu_time[client_id][stage_id][instance_id] = Gauge('process_cpu_time_'+str(self.prom_id), "CPU-Time of process in user mod of "+str(instance_id))
                 self.prom_id += 1
-            if not (client_id in self.proc_mem):
-                self.proc_mem[client_id] = {}
-            if not (service_id in self.proc_mem[client_id]):
-                self.proc_mem[client_id][service_id] = {}
-            if not (instance_id in self.proc_mem[client_id][service_id]):
-                self.proc_mem[client_id][service_id][instance_id] = Gauge('process_memory_'+str(self.prom_id), "Memory usage of the process of "+str(instance_id))
-                self.prom_id += 1
-            self.proc_cpu_time[client_id][service_id][instance_id].set(mess['cpu-time-user'])
-            self.proc_mem[client_id][service_id][instance_id].set(mess['memory-rss'])
-        mess_df = pd.DataFrame(mess, index=[0]) 
-        if  app_metric != None:
-            mess_df.to_csv('app_monitor.csv', mode='a', index=False, header= not file_exists('app_monitor.csv'))
-        else:
+            cpu_key = list(process_metric.keys())[0]
+            self.proc_cpu_time[client_id][stage_id][instance_id].set(process_metric[cpu_key]['user'])
+            mess_df = pd.DataFrame(process_metric[cpu_key], index=[0]) 
             mess_df.to_csv('process_monitor.csv', mode='a', index=False, header= not file_exists('process_monitor.csv'))
-        print(mess_df)
-        # print(mess)
+        
+        # Above code report 'cpu stats' to Prometheus
+        # TO DO:
+        # Report 'memory stats' to Prometheus 
+
         
 
-
-
-
 start_http_server(8989)
-connetor_conf = utils.load_config("./collector.json")
+connetor_conf = utils.load_config("./collector.yaml")
 monitor_col = Monitoring_collector()
-client = Amqp_Collector(connetor_conf['amqp_collector']['conf'],monitor_col)
+client = Amqp_Collector(connetor_conf['collector'],monitor_col)
 client.start()
 
 
