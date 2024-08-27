68567e79de98c148836857057df5e70ab809796b
Tri Nguyen <tri.m.nguyen@aalto.fi>
2024-08-27 14:30:56 +0300

    update MLServing tutorial

--- tutorials/MLServing/client/client.py
+++ tutorials/MLServing/client/client.py
@@ -1,61 +1,42 @@
 import requests,time, argparse
-from threading import Thread
 import os, random
-
-import qoa4ml.qoaUtils as qoa_utils
-from qoa4ml.QoaClient import QoaClient
-
+from threading import Timer
+from threading import Lock
+lock = Lock()
+import logging
+logging.basicConfig(format="%(asctime)s:%(levelname)s -- %(message)s", level=logging.INFO)
 
 parser = argparse.ArgumentParser(description="Data processing")
-parser.add_argument('--th', help='number concurrent thread', default=5)
-parser.add_argument('--sl', help='time sleep', default=1.0)
+parser.add_argument('--f', help='frequency', default=5)
 parser.add_argument('--conf', help='time sleep', default="./qoa_config.yaml")
 args = parser.parse_args()
-concurrent = int(args.th)
-time_sleep = float(args.sl)
+frequency = int(args.f)
 config_file = args.conf
 
-
-qoaConfig = qoa_utils.load_config(file_path=config_file)
-try:
-    qoaClient = QoaClient(qoaConfig['qoa_config'])
-except:
-    print("Unable to init qoa client")
-
+requesting_interval = 1.0 / frequency
 
 url = 'http://web-service:5000/inference'
 
-def responsetime_timer():
-    if "qoaClient" in globals():
-        qoaClient.timer()
+start_time = time.time()
+counter = 0
 
-def qoa_report():
-    if "qoaClient" in globals():
-        report = qoaClient.report(submit=True)
-        print(report)
-
-
-
-
-def sender(num_thread):
-    count = 0
-    start_time = time.time()
-    while (time.time() - start_time < 600):
-        print("This is thread: ",num_thread, "Starting request: ", count)
-        ran_file = random.choice(os.listdir("./image"))
-        files = {'image': open("./image/"+ran_file, 'rb')}
-
-        responsetime_timer()
+def send_inference_request():
+    global counter
+    with lock:
+        request_no = counter
+        counter += 1
+    timer = Timer(requesting_interval, send_inference_request)
+    
+    if (time.time() - start_time < 600):
+        timer.start()
+    
+    ran_file = random.choice(os.listdir("./image"))
+    files = {'image': open("./image/"+ran_file, 'rb')}
+    logging.info(f"Sending request {request_no}")
+    try:
         response = requests.post(url, files=files)
-        responsetime_timer()
-        qoa_report()
-        
-
-
-        print("Thread - ",num_thread, " Response:" ,response.content)
-        count += 1
-        time.sleep(time_sleep)
-
-for i in range(concurrent):
-    t = Thread(target=sender,args=[i])
-    t.start()
+        logging.info(f"Response {request_no}: {response.content}")
+    except Exception as e:
+        logging.error(f"Error at request {request_no}: {e.with_traceback}")
+    
+send_inference_request()
